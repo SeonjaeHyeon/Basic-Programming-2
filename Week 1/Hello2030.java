147db76d37fd40d63a9baf61ee72cdfb0c96a71c
Seonjae Hyeon <hsj106@mju.ac.kr>
2020-09-16 01:45:11 +0900

    Fix encoding error:  ISO-8859-1 -> UTF-8

--- Week 1/Hello2030.java
+++ Week 1/Hello2030.java
@@ -4,7 +4,7 @@ public class Hello2030 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = 2030;
-		System.out.println("Çï·Î"+n);
+		System.out.println("í—¬ë¡œ"+n);
 	}
 
 }
