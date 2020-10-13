8de6ff6b08f2171cbe4241e4c3f4e8dcab50110f
SeonjaeHyeon <hsj106@mju.ac.kr>
2020-10-13 19:00:01 +0900

    Add Exercise 5-5 ~ 5-8

---
+++ Exercise/ch5exercise8.java
@@ -0,0 +1,69 @@
+// Exercise 5-8
+
+class Point {
+    private int x, y;
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    protected void move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class PositivePoint extends Point {
+    public PositivePoint() {
+        super(0, 0);
+    }
+
+    public PositivePoint(int x, int y) {
+        super(0, 0);
+
+        if(x < 0) {
+            x = 0;
+        }
+        
+        if(y < 0) {
+            y = 0;
+        }
+
+        super.move(x, y);
+    }
+
+    public void move(int x, int y) {
+        if(x < 0 || y < 0) {
+            return;
+        }
+
+        super.move(x, y);
+    }
+
+    public String toString() {
+        return "(" + getX() + "," + getY() + ")의 점";
+    }
+}
+
+public class ch5exercise8 {
+    public static void main(String[] args) {
+        PositivePoint p = new PositivePoint();
+        p.move(10, 10);
+        System.out.println(p.toString() + "입니다.");
+
+        p.move(-5, 5);
+        System.out.println(p.toString() + "입니다.");
+
+        PositivePoint p2 = new PositivePoint(-10, -10);
+        System.out.println(p2.toString() + "입니다.");
+    }
+}
