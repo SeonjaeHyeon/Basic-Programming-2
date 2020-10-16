def3dd788c9ecebbb84f46294f08579222aca890
SeonjaeHyeon <hsj106@mju.ac.kr>
2020-10-16 21:19:43 +0900

    Add Exercise 1-1 ~ 1-2

---
+++ Exercise/ch1exercise2.java
@@ -0,0 +1,8 @@
+// Exercise 1-2
+
+public class ch1exercise2 {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+        System.out.println("재밌어서 미안합니다~~");
+    }
+}
