caa7acd996b2c7f540ee72ab917be611c1364411
SeonjaeHyeon <hsj106@mju.ac.kr>
2020-10-16 21:19:00 +0900

    Add Open Challenge 1

---
+++ Exercise/ch1challenge.java
@@ -0,0 +1,9 @@
+// Open Challenge 1
+
+public class ch1challenge {
+    public static void main(String[] args) {
+        System.out.println("Seonjae Hyeon");
+        System.out.println("20 years old");
+        System.out.println("Department of Convergence Software Studies");
+    }    
+}
